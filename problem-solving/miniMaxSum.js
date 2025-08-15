b2cf69463297a67afb71faa85482c19bcc968c5e
Gabriel Ninow <gabrielninow@gmail.com>
2025-08-14 22:25:53 -0300

    added miniMaxSum

---
+++ problem-solving/miniMaxSum.js
@@ -0,0 +1,30 @@
+const numbersList = [4,7,8,1,3];
+
+function miniMaxSum(arr) {
+    let sortedArray = arr.toSorted((a, b) => a - b);
+
+    console.log(`${getMin(sortedArray)} ${getMax(sortedArray)}`)
+    
+}
+
+function getMin(arr){
+    let min = 0;
+
+    for(let i = 0; i <= arr.length-2; i++){
+        min += arr[i];
+    }
+
+    return min;
+}
+
+function getMax(arr){
+    let max = 0;
+
+    for(let i = arr.length-1; i > 0; i--){
+        max += arr[i];
+    }
+
+    return max;
+}
+
+miniMaxSum(numbersList)
